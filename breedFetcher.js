4853748c6259986fc87cccf14023761ccfa67f85
Jonathan Phair <jmphair@gmail.com>
2023-01-19 15:08:16 -0500

    the array from the api contains an object, can access it with index 0 and dot notation

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
 // use request library to make http request
 const request = require('request');
+// use process.argv for user input which will be at index 2, consider that input = [node, breedFetcher.js, BREED]
+const breedName = process.argv[2];
 
 // using the request function right out of the fetcher.js file but repurposing it
-request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
+
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  // consider that the data variable is literally sitting in an array on the api site
+  // then we can use dot notation to access the description 
+  console.log(data[0].description);
 
 });
 
