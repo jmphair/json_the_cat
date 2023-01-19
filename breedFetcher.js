02bba3620ad3eb8a06e503f3031e05bcb86ebb10
Jonathan Phair <jmphair@gmail.com>
2023-01-19 16:13:09 -0500

    ok refactoring complete

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,27 @@
-// use request library to make http request
 const request = require('request');
-// use process.argv for user input which will be at index 2, consider that input = [node, breedFetcher.js, BREED]
 const breedName = process.argv[2];
 
-// using the request function right out of the fetcher.js file but repurposing it
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  if (error) {
-    console.log('Error: ', error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log(`The ${breedName} can't be found`);
-    return;
-  }
-  if (data[0]) {
-    console.log(`Breed Description: ${data[0].description}`);
-    return;
-  }
+  request(url, (error, response, body) => {
 
-});
+    if (error) {
+      callback('Error: ', null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      callback(`The ${breedName} can't be found`, null);
+      return;
+    }
+    if (data[0]) {
+      callback(null, data[0].description);
+    }    
 
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
