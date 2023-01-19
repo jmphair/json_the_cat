64e85e1f6b30e29b559c52fbb21114e793a1c941
Jonathan Phair <jmphair@gmail.com>
2023-01-19 14:49:04 -0500

    json parse returns an actual object

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,12 @@
-const request = require('request'); // use request library to make http request
+// use request library to make http request
+const request = require('request');
 
+// using the request function right out of the fetcher.js file but repurposing it
 request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
-  console.log(typeof body);
+  
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
 
 });
 
